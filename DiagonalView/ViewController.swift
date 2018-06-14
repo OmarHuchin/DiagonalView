c20d3ea675390e74dc84776ccd52243371f7bb67
Felix Omar Huchin Rodriguez <fhuchin@palaceresorts.com>
2018-06-13 21:48:46 -0500

    First Inital

--- DiagonalView/ViewController.swift
+++ DiagonalView/ViewController.swift
@@ -23,3 +23,64 @@ class ViewController: UIViewController {
 
 }
 
+@IBDesignable
+class AngleView: UIView {
+    
+    @IBInspectable var fillColor: UIColor = UIColor.blue { didSet { setNeedsLayout() } }
+    @IBInspectable var angleDirection : Int = 0
+        /*{didSet(newValue){
+            if newValue == 0{
+                points = leftPoints
+            }else{
+                points = rightPoints
+            }
+            setNeedsLayout()
+        }}*/
+    var leftPoints = [
+        CGPoint(x: 1, y: 0),
+        CGPoint.zero,
+        CGPoint(x: 0, y: 1),
+        CGPoint(x: 1, y: 0.5)
+    ]
+    var rightPoints = [
+        CGPoint.zero,
+        CGPoint(x: 1, y: 0),
+        CGPoint(x: 1, y: 1),
+        CGPoint(x: 0, y: 0.5)
+    ]
+    var points =  [
+        CGPoint(x: 1, y: 0),
+        CGPoint.zero,
+        CGPoint(x: 0, y: 0.7),
+        CGPoint(x: 1, y: 0.3)
+        ]{ didSet(newValue) {
+            setNeedsLayout()
+        }
+    }
+    
+   
+    private lazy var shapeLayer: CAShapeLayer = {
+        let _shapeLayer = CAShapeLayer()
+        self.layer.insertSublayer(_shapeLayer, at: 0)
+        return _shapeLayer
+    }()
+
+    override func layoutSubviews() {
+        shapeLayer.fillColor = fillColor.cgColor
+        
+        let path = UIBezierPath()
+        
+        path.move(to: convert(relativePoint: points[0]))
+        for point in points.dropFirst() {
+            path.addLine(to: convert(relativePoint: point))
+        }
+        path.close()
+        
+        shapeLayer.path = path.cgPath
+    }
+    
+    private func convert(relativePoint point: CGPoint) -> CGPoint {
+        return CGPoint(x: point.x * bounds.width + bounds.origin.x, y: point.y * bounds.height + bounds.origin.y)
+    }
+}
+
